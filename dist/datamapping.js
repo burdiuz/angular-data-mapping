10ec97df4562d2e2a18aabe13ea4ac1c7004621e
Oleg Galaburda <burdiuz@gmail.com>
2015-03-02 13:48:51 -0500

    build

--- dist/datamapping.js
+++ dist/datamapping.js
@@ -83,7 +83,7 @@ function createEntity(data, constructor, entityTypeMap) {
       constructor = data.constructor || data.__proto__.constructor;
     } else if ("$constructor" in data && data["$constructor"]) {
       try {
-        eval("try{ constructor = window." + data["$constructor"] + "; }catch(error){}");
+        eval("constructor = window." + data["$constructor"]);
       } catch (error) {
         console.log(error);
         throw new Error('Entity class "'+data["$constructor"]+'" is not defined.');
